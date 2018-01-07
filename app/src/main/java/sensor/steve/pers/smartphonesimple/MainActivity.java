f3ba60e262ef9e1c01c4194a986cf6a32a2c3677
steve <551619855@qq.com>
2018-01-07 15:14:00 +0800

    a

--- app/src/main/java/sensor/steve/pers/smartphonesimple/MainActivity.java
+++ app/src/main/java/sensor/steve/pers/smartphonesimple/MainActivity.java
@@ -2,8 +2,31 @@ package sensor.steve.pers.smartphonesimple;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    //--- ble
+    TextView bleResText = (TextView) findViewById(R.id.bleResText);
+    Button bleButton = (Button) findViewById(R.id.bleButton);
+
+    //-- imu
+    Spinner sensorSpeedSpinner=(Spinner) findViewById(R.id.sensorSpeed);
+    TextView sensorResultText = (TextView) findViewById(R.id.sensorResultText);
+    Button imuButton = (Button) findViewById(R.id.imuButton);
+
+
+    //-- tcp
+    EditText ipText = (EditText) findViewById(R.id.ipText);
+    EditText portText = (EditText) findViewById(R.id.portText);
+    Button tcpButton = (Button) findViewById(R.id.tcpButton);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
